bd4ac83065e2f2cadbc3d0880288cbc323a34549
Sanith   Bimsara <77660641+sanith2005@users.noreply.github.com>
2021-07-03 11:18:38 -0700

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -17,8 +17,7 @@ async def start(bot, message):
         buttons = [[
             InlineKeyboardButton('Search Here', switch_inline_query_current_chat=''),
             InlineKeyboardButton('Go Inline', switch_inline_query=''),
-        ]],
-        [[
+        ]],[[
             InlineKeyboardButton('Search Here', switch_inline_query_current_chat=''),
             InlineKeyboardButton('Go Inline', switch_inline_query=''),
         ]]
