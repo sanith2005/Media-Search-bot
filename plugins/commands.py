02019680c22b726bf66da2cfc91e58ec2b1cf4e7
Sanith   Bimsara <77660641+sanith2005@users.noreply.github.com>
2021-07-03 04:29:20 -0700

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -17,6 +17,8 @@ async def start(bot, message):
         buttons = [[
             InlineKeyboardButton('Search Here', switch_inline_query_current_chat=''),
             InlineKeyboardButton('Go Inline', switch_inline_query=''),
+        ]],[[
+            InlineKeyboardButton("Contact", url="telegram.me/SBS_Studio"),
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply(START_MSG, reply_markup=reply_markup)
