4c13efc87334509f8a7411226ba6e1a13f08a1cc
Sanith   Bimsara <77660641+sanith2005@users.noreply.github.com>
2021-07-02 23:32:10 -0700

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -44,7 +44,7 @@ async def answer(bot, query):
             InlineQueryResultCachedDocument(
                 title=file.file_name,
                 file_id=file.file_id,
-                caption=file.caption or "",
+                caption=file."Upload From @myKdrama_bot.",
                 description=f'Size: {get_size(file.file_size)}\nType: {file.file_type}',
                 reply_markup=reply_markup))
 
