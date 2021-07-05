ff7e6e1fab3699df9dcc2d35181b5631e3c57f10
Sanith   Bimsara <77660641+sanith2005@users.noreply.github.com>
2021-07-05 04:24:59 -0700

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -33,18 +33,6 @@ def help_user(bot, update):
         disable_web_page_preview=True,
         reply_to_message_id=update.message_id
     )
-    
-@Client.on_message(filters.command(["support"]))
-def help_user(bot, update):
-
-    bot.send_message(
-        chat_id=update.chat.id,
-        text='Join Now . . .',
-        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="JOIN NOW", url=f"telegram.me/{MY_CHANNEL}")]]),
-        parse_mode="html",
-        disable_web_page_preview=True,
-        reply_to_message_id=update.message_id
-    )
 
     
 @Client.on_message(filters.command('channel') & filters.user(ADMINS))
